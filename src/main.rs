5de616f1adbd65b5cfe81fae0a749037ad73581c
Jake Carrington AO <jake.carrington@ao.com>
2020-01-06 16:11:52 +0000

    More opcodes

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,9 @@ mod io_operations;
 use io_operations::IOOperation;
 
 fn main() {
-    let mut memory: [u8; 0x1000] = [0; 0x1000];
+    let mut memory: [usize; 0x1000] = [0; 0x1000];
     let mut registers: [u8; 16] = [0; 16];
-    let mut I: u16 = 0;
+    let mut I: usize = 0;
     let mut stack: [u8; 48] = [0; 48];
     let mut delay_timer: usize = 0;
     let mut sound_timer: usize = 0;
@@ -56,6 +56,132 @@ fn main() {
         let y = nibbles.2 as usize;
         let n = nibbles.3 as usize;
 
+        let mut sprites: [[u8; 5]; 16] = [[0; 5]; 16];
+        let sprite0 = [0xF0, 0x90, 0x90, 0x90, 0xF0];
+        let sprite1 = [
+            0x20,
+            0x60,
+            0x20,
+            0x20,
+            0x70,
+        ];
+        let sprite2 = [
+            0xF0,
+            0x10,
+            0xF0,
+            0x80,
+            0xF0,
+        ];
+        let sprite3 = [
+            0xF0,
+            0x10,
+            0xF0,
+            0x10,
+            0xF0,
+        ];
+        let sprite4 = [
+            0x90,
+            0x90,
+            0xF0,
+            0x10,
+            0x10,
+        ];
+        let sprite5 = [
+            0xF0,
+            0x80,
+            0xF0,
+            0x10,
+            0xF0,
+        ];
+        let sprite6 = [
+            0xF0,
+            0x80,
+            0xF0,
+            0x90,
+            0xF0,
+        ];
+        let sprite7 = [
+            0xF0,
+            0x10,
+            0x20,
+            0x40,
+            0x40,
+        ];
+        let sprite8 = [
+            0xF0,
+            0x90,
+            0xF0,
+            0x90,
+            0xF0,
+        ];
+        let sprite9 = [
+            0xF0,
+            0x90,
+            0xF0,
+            0x10,
+            0xF0,
+        ];
+        let spriteA = [
+            0xF0,
+            0x90,
+            0xF0,
+            0x90,
+            0x90,
+        ];
+        let spriteB = [
+            0xE0,
+            0x90,
+            0xE0,
+            0x90,
+            0xE0,
+        ];
+        let spriteC = [
+            0xF0,
+            0x80,
+            0x80,
+            0x80,
+            0xF0,
+        ];
+        let spriteD = [
+            0xE0,
+            0x90,
+            0x90,
+            0x90,
+            0xE0,
+        ];
+        let spriteE = [
+            0xF0,
+            0x80,
+            0xF0,
+            0x80,
+            0xF0,
+        ];
+        let spriteF = [
+            0xF0,
+            0x80,
+            0xF0,
+            0x80,
+            0x80,
+        ];
+        sprites[0x00] = sprite0;
+        sprites[0x01] = sprite1;
+        sprites[0x02] = sprite2;
+        sprites[0x03] = sprite3;
+        sprites[0x04] = sprite4;
+        sprites[0x05] = sprite5;
+        sprites[0x06] = sprite6;
+        sprites[0x07] = sprite7;
+        sprites[0x08] = sprite8;
+        sprites[0x09] = sprite9;
+        sprites[0x0A] = spriteA;
+        sprites[0x0B] = spriteB;
+        sprites[0x0C] = spriteC;
+        sprites[0x0D] = spriteD;
+        sprites[0x0E] = spriteE;
+        sprites[0x0F] = spriteF;
+
+        
+
         match nibbles {
             (0x00, 0x00, 0x0e, 0x00) => {
                 display_buffer_front = [[0; 64]; 32];
@@ -78,9 +204,104 @@ fn main() {
                     program_counter += 2;
                 }
             },
-            (0x06, _, _, _) => println!("{} {}", x, kk),
+            (0x04, _, _, _) => {
+                if registers[x] != kk {
+                    program_counter += 2;
+                }
+            },
+            (0x05, _, _, 0x00) => {
+                if registers[x] == registers[y] {
+                    program_counter += 2;
+                }
+            }
+            (0x06, _, _, _) => {
+                registers[x] = kk;
+            },
+            (0x07, _, _, _) => {
+                registers[x] = registers[x] + kk;
+            },
+            (0x08, _, _, 0x00) => {
+                registers[x] = registers[y];
+            },
+            (0x08, _, _, 0x01) => {
+                registers[x] = registers[x] | registers[y];
+            },
+            (0x08, _, _, 0x02) => {
+                registers[x] = registers[x] & registers[y];
+            },
+            (0x08, _, _, 0x03) => {
+                registers[x] = registers[x] ^ registers[y];
+            },
+            (0x08, _, _, 0x04) => {
+                if registers[x] + registers[y] > 255 { 
+                    registers[0x0F] = 1;
+                } else {
+                    registers[0x0F] = 0;
+                }
+                registers[x] = registers[x] + registers[y] as u8;
+            },
+            (0x08, _, _, 0x05) => {
+                if registers[x] > registers[y] { 
+                    registers[0x0F] = 1;
+                    registers[x] = registers[x] - registers[y] as u8;
+                } else {
+                    registers[0x0F] = 0;
+                }
+            },
+            (0x08, _, _, 0x06) => {
+                registers[0x0F] = registers[x] & 0x000F;
+                registers[x] = registers[x] / 2;
+            },
+            (0x08, _, _, 0x07) => {
+                if registers[y] > registers[x] {
+                    registers[0x0F] = 1;
+                } else {
+                    registers[0x0F] = 0;
+                }
+
+                registers[x] = registers[y] - registers[x];
+            }, 
+            (0x08, _, _, 0x0E) => {
+                registers[0x0F] = registers[x] & 0x08;
+                registers[x] = registers[x] * 2;
+            },
+            (0x09, _, _, 0) => {
+                if registers[x] != registers[7] {
+                    program_counter += 2;
+                }
+            },
+            (0x0A, _, _, _) => {
+                I = nnn;
+            },
+            (0x0F, _, 0x03, 0x03) => {
+                let digits: Vec<_> = registers[x].to_string().chars().map(|d| d.to_digit(10).unwrap()).collect();
+                match digits.as_slice() {
+                    [h, t, o] => {
+                        memory[I] = *h as usize;
+                        memory[I+1] = *t as usize;
+                        memory[I+2] = *o as usize;
+                    },
+                    [t, o] => {
+                        memory[I+1] = *t as usize;
+                        memory[I+2] = *o as usize;
+                    }, 
+                    [o] => {
+                        memory[I+2] = *o as usize;
+                    }
+                    _ => panic!("This should never happen!"),
+                } 
+            },
+            (0x0F, _, 0x06, 0x05) => {
+                for i in 0..x {
+                    registers[i] = memory[I+i] as u8;
+                }
+            },
+            (0x0F, _, 0x02, 0x09) => {}
             _ => println!("OpCode Not Found!"),
         }
+        println!("PC: {}", program_counter);
+        println!("Registers: {:?}", registers);
+        println!("Register I: {}", I);
     }
 
     // let mut events_loop = glium::glutin::EventsLoop::new();
